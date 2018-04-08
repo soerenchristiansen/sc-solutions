197bcbb86d7f31049d941c806dd80cada078777d
Soren Christiansen <sch@vertica.dk>
2018-04-08 22:28:15 +0200

    consolidate

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const cons = require('consolidate');
 
 express()
   .use(express.static(path.join(__dirname, 'images')))
+  .engine('html', cons.swig)
   .set('view engine', 'html')
   .get('/', (req, res) => res.render('/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
