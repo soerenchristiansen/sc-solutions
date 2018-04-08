ce7f281f64d5ffedf42a12bb0c9c57fd6b0deeaf
Soren Christiansen <sch@vertica.dk>
2018-04-08 22:24:00 +0200

    hmmm

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'images')))
+  .set('view engine', 'html')
   .get('/', (req, res) => res.render('/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
