632a0f40dc845cc1db3639db935221c016a1dc2e
Soren Christiansen <sch@vertica.dk>
2018-04-09 20:19:12 +0200

    set views

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cons = require('consolidate');
 express()
   .use(express.static(path.join(__dirname, 'images')))
   .engine('html', cons.swig)
+  .set('views', path.join(__dirname, 'app'))
   .set('view engine', 'html')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
