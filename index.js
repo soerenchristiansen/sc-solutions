283dd7189f8df87c3a9622d4a37b0a19081d80d3
Soren Christiansen <sch@vertica.dk>
2018-04-09 20:27:39 +0200

    removed consolidate

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-// const cons = require('consolidate');
 
 express()
   .use(express.static(path.join(__dirname, 'images')))
