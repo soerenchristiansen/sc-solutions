827637eefe4de1f52be48efb0c57e3f3cb398dbc
Soren Christiansen <sch@vertica.dk>
2018-04-09 20:22:18 +0200

    remove

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-const cons = require('consolidate');
+// const cons = require('consolidate');
 
 express()
   .use(express.static(path.join(__dirname, 'images')))
-  .engine('html', cons.swig)
+  // .engine('html', cons.swig)
   .set('views', path.join(__dirname, 'app'))
   .set('view engine', 'html')
   .get('/', (req, res) => res.render('index'))
