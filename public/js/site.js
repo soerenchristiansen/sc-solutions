a41d60f39e1628891cf23b2ec918eeee908691b8
Soren Christiansen <sch@vertica.dk>
2018-04-10 13:42:58 +0200

    added scrollspy

--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,22 @@
 $(function() {
     "use strict";
-
     
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var e = $(this.hash);
+            if ((e = e.length ? e : $("[name=" + this.hash.slice(1) + "]")).length)
+                return $("html, body").animate({ scrollTop: e.offset().top - 57 }, 1e3, "easeInOutExpo"), !1;
+        }
+    }),
+    $(".js-scroll-trigger").click(function() {
+        $(".navbar-collapse").collapse("hide")
+    }),
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 57
+    });
+    changeMenuDesignOnScroll();
+    $(window).scroll(changeMenuDesignOnScroll);
 });
 
 function includeHTML() {
@@ -15,4 +30,8 @@ function includeHTML() {
             $(element).load(htmlFile);
         }
     }
+}
+
+function changeMenuDesignOnScroll() {
+    $("#mainNav").offset().top > 100 ? $("#mainNav").addClass("navbar-shrink") : $("#mainNav").removeClass("navbar-shrink");
 }
